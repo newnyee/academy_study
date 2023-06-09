101cb86d208e127fba0e420ce6e9192e5a0f073f
newny <newny@gmail.com>
2023-06-09 18:23:08 +0900

    20230609

--- workspace_spring/spring09_myshop/src/main/java/kr/co/itwill/Spring09MyshopApplication.java
+++ workspace_spring/spring09_myshop/src/main/java/kr/co/itwill/Spring09MyshopApplication.java
@@ -3,11 +3,9 @@ package kr.co.itwill;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
